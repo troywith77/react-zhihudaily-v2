af378979830626a4eb82b1eb3db6a8d9d310d96c
troywith77 <ruitang307@gmail.com>
2018-03-14 18:07:11 +0800

    append last commit's comment

--- src/reducers/themes.js
+++ src/reducers/themes.js
@@ -1,9 +1,10 @@
-let initialState = {
+const initialState = {
   fetching: false,
   themes: {}
 };
 
 try {
+  // 从 localStorage 里取出 themes，如果存在的话
   initialState.themes = JSON.parse(localStorage.themes || '{}');
 } catch (err) {
   console.log(err);
