2af3d442d82395f78cd152da7c7114d93de7aa35
troywith77 <ruitang307@gmail.com>
2018-03-15 17:23:20 +0800

    update material list button

--- src/components/StoryList/StoryList.js
+++ src/components/StoryList/StoryList.js
@@ -6,44 +6,50 @@ import List, {
   ListSubheader
 } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import { convertImageSrc } from '~/services/utils';
 import './StoryListStyle';
 
-export default function StoryList ({
-  stories,
-  header
-}) {
-  if (!stories.length) return null;
-  return (
-    <div className="story-list">
-      <List
-        dense={false}
-        subheader={<ListSubheader component="div" className="list-subheader">{header}</ListSubheader>}
-      >
-        {
-          stories.map((story) => {
-            const path = story.images && story.images[0]
-            let imgSrc
-            if (path) imgSrc = convertImageSrc(path);
-            return (
-              <ListItem key={story.id}>
-                {
-                  imgSrc && (
-                  <ListItemAvatar>
-                    <Avatar alt={story.title} src={imgSrc} />
-                  </ListItemAvatar>
-                  )
-                }
-                <ListItemText
-                  primary={<Link to={`/story/${story.id}`}>{story.title}</Link>}
-                />
-              </ListItem>
-            )
-          })
-        }
-      </List>
-    </div>
-  )
+class StoryList extends React.Component {
+  handleRouteChange = (id) => {
+    // 等待 material list 点击效果结果再跳转，看起来比较舒服
+    setTimeout(() => {
+      this.props.history.push(`/story/${id}`)
+    }, 300)
+  }
+
+  render () {
+    const { stories, header } = this.props;
+    if (!stories.length) return null;
+    return (
+      <div className="story-list">
+        <List subheader={<ListSubheader component="div" className="list-subheader">{header}</ListSubheader>}>
+          {
+            stories.map((story) => {
+              const path = story.images && story.images[0]
+              let imgSrc
+              if (path) imgSrc = convertImageSrc(path);
+              return (
+                <ListItem key={story.id} button onClick={() => this.handleRouteChange(story.id)}>
+                  {
+                    imgSrc && (
+                    <ListItemAvatar>
+                      <Avatar alt={story.title} src={imgSrc} />
+                    </ListItemAvatar>
+                    )
+                  }
+                  <ListItemText
+                    primary={story.title}
+                  />
+                </ListItem>
+              )
+            })
+          }
+        </List>
+      </div>
+    )
+  }
 }
+
+export default withRouter(StoryList);
