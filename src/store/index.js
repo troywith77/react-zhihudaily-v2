9e47e1ffcc7d5cea0346fa5d246b90cdfd4ec21c
troywith77 <ruitang307@gmail.com>
2018-03-20 09:59:17 +0800

    added redux logger in development

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import { routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 
@@ -11,21 +11,21 @@ import LS from './middlewares/localStorage';
 export default () => {
   const history = createHistory();
 
+  // redux devtools set up
   const composeEnhancers =
     typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
       }) : compose;
 
-  const enhancer = composeEnhancers(
-    applyMiddleware(
-      routerMiddleware(history),
-      thunk,
-      LS
-      // logger
-    )
-  );
+  // middlewares
+  let middlewares = [routerMiddleware(history), thunk, LS];
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares = [...middlewares, logger];
+  }
+  const enhancer = composeEnhancers(applyMiddleware(...middlewares));
 
+  // get data persisted in localStorage
   const persistedState = getPersistedState();
 
   const store = createStore(
@@ -33,12 +33,15 @@ export default () => {
     persistedState,
     enhancer
   )
+
+  // hot reload redux
   if (module.hot) {
     module.hot.accept('~/reducers', () => {
       const nextRootReducer = require('~/reducers').default;
       store.replaceReducer(nextRootReducer);
     });
   }
+
   return {
     store,
     history
